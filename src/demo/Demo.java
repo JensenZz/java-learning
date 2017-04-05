702cd77da02eb5288c03a1c5063d47299902dd27
zhajinxing <331071873@qq.com>
2017-04-05 14:11:48 +0800

    aaa

---
+++ src/demo/Demo.java
@@ -0,0 +1,16 @@
+package demo;
+
+/**
+ * <p>Title: 类的名称</p>
+ * <p>Description: 类的实现描述<p>
+ * <p>Copyright: Copyright (c) 2017</p>
+ * <p>Company:人人行科技 </p>
+ *
+ * @author JensenZz
+ * @version 1.0
+ * @date 2017/4/5
+ */
+
+public class Demo {
+
+}
