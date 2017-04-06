5664958db23d067b66c01a381bf917ca398e6d78
zhajinxing <331071873@qq.com>
2017-04-06 10:17:02 +0800

    Merge branches 'dev2' and 'dev3'
    # Conflicts:
    #	src/java7/Test.java

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,5 +1,9 @@
 package demo;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <p>Title: 类的名称</p>
  * <p>Description: 类的实现描述<p>
@@ -12,5 +16,47 @@ package demo;
  */
 
 public class Demo {
-//sadasd
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("/Users/JensenZz/Desktop/demo.txt", true);
+            for (int i = 1; i <= 1000; i++) {
+                if (i % 5 == 0) {
+                    writer.write(i + "\n");
+                }
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<String> strings = new ArrayList<String>();
+        File file = new File("/Users/JensenZz/Desktop/demo.txt");
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                if (null != tempString && !tempString.isEmpty()) {
+                    strings.add(tempString);
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+        for (String string : strings) {
+            System.out.println(string);
+        }
+        System.out.println(strings.size());
+
+    }
+
 }
